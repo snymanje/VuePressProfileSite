78bbf2c8c3e816cb0e1510fc8215984135a5e031
jeansn <jeansn@tfg.co.za>
2020-05-29 10:27:14 +0200

    added sidebar to articles

--- .vuepress/components/blogs.vue
+++ .vuepress/components/blogs.vue
@@ -1,238 +1,96 @@
 <template>
   <section class="text-gray-700 body-font">
     <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap -m-4">
-        <div class="p-4 md:w-1/3">
-          <div
-            class="h-full border-2 border-gray-200 rounded-lg overflow-hidden"
-          >
-            <img
-              class="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="https://dummyimage.com/720x400"
-              alt="blog"
-            />
-            <div class="p-6">
+      <div class="flex flex-col text-center w-full mb-20">
+        <h2
+          class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1"
+        >ROOF PARTY POLAROID</h2>
+        <h1
+          class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+        >Master Cleanse Reliac Heirloom</h1>
+        <p
+          class="lg:w-2/3 mx-auto leading-relaxed text-base"
+        >Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      </div>
+      <div class="flex flex-wrap -mx-4 -my-8">
+        <div class="py-8 px-4 lg:w-1/3">
+          <div class="h-full flex items-start">
+            <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+              <span class="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">Jul</span>
+              <span class="font-medium text-xl text-gray-800 title-font">18</span>
+            </div>
+            <div class="flex-grow pl-6">
               <h2
-                class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1"
-              >
-                CATEGORY
-              </h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                The Catalyzer
-              </h1>
-              <p class="leading-relaxed mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <div class="flex items-center flex-wrap ">
-                <a
-                  class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0"
-                  >Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-                <span
-                  class="text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-300"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-                    ></path>
-                    <circle cx="12" cy="12" r="3"></circle></svg
-                  >1.2K
+                class="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1"
+              >CATEGORY</h2>
+              <h1 class="title-font text-xl font-medium text-gray-900 mb-3">The 400 Blows</h1>
+              <p
+                class="leading-relaxed mb-5"
+              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+              <a class="inline-flex items-center">
+                <img
+                  alt="blog"
+                  src="https://dummyimage.com/103x103"
+                  class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
+                />
+                <span class="flex-grow flex flex-col pl-3">
+                  <span class="title-font font-medium text-gray-900">Alper Kamu</span>
                 </span>
-                <span
-                  class="text-gray-600 inline-flex items-center leading-none text-sm"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                    ></path></svg
-                  >6
-                </span>
-              </div>
+              </a>
             </div>
           </div>
         </div>
-        <div class="p-4 md:w-1/3">
-          <div
-            class="h-full border-2 border-gray-200 rounded-lg overflow-hidden"
-          >
-            <img
-              class="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="https://dummyimage.com/721x401"
-              alt="blog"
-            />
-            <div class="p-6">
+        <div class="py-8 px-4 lg:w-1/3">
+          <div class="h-full flex items-start">
+            <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+              <span class="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">Jul</span>
+              <span class="font-medium text-xl text-gray-800 title-font">18</span>
+            </div>
+            <div class="flex-grow pl-6">
               <h2
-                class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1"
-              >
-                CATEGORY
-              </h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                The 400 Blows
-              </h1>
-              <p class="leading-relaxed mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <div class="flex items-center flex-wrap">
-                <a
-                  class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0"
-                  >Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-                <span
-                  class="text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-300"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-                    ></path>
-                    <circle cx="12" cy="12" r="3"></circle></svg
-                  >1.2K
+                class="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1"
+              >CATEGORY</h2>
+              <h1 class="title-font text-xl font-medium text-gray-900 mb-3">Shooting Stars</h1>
+              <p
+                class="leading-relaxed mb-5"
+              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+              <a class="inline-flex items-center">
+                <img
+                  alt="blog"
+                  src="https://dummyimage.com/102x102"
+                  class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
+                />
+                <span class="flex-grow flex flex-col pl-3">
+                  <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
                 </span>
-                <span
-                  class="text-gray-600 inline-flex items-center leading-none text-sm"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                    ></path></svg
-                  >6
-                </span>
-              </div>
+              </a>
             </div>
           </div>
         </div>
-        <div class="p-4 md:w-1/3">
-          <div
-            class="h-full border-2 border-gray-200 rounded-lg overflow-hidden"
-          >
-            <img
-              class="lg:h-48 md:h-36 w-full object-cover object-center"
-              src="https://dummyimage.com/722x402"
-              alt="blog"
-            />
-            <div class="p-6">
+        <div class="py-8 px-4 lg:w-1/3">
+          <div class="h-full flex items-start">
+            <div class="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+              <span class="text-gray-500 pb-2 mb-2 border-b-2 border-gray-300">Jul</span>
+              <span class="font-medium text-xl text-gray-800 title-font">18</span>
+            </div>
+            <div class="flex-grow pl-6">
               <h2
-                class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1"
-              >
-                CATEGORY
-              </h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                Shooting Stars
-              </h1>
-              <p class="leading-relaxed mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <div class="flex items-center flex-wrap ">
-                <a
-                  class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0"
-                  >Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-                <span
-                  class="text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-300"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-                    ></path>
-                    <circle cx="12" cy="12" r="3"></circle></svg
-                  >1.2K
+                class="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1"
+              >CATEGORY</h2>
+              <h1 class="title-font text-xl font-medium text-gray-900 mb-3">Neptune</h1>
+              <p
+                class="leading-relaxed mb-5"
+              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+              <a class="inline-flex items-center">
+                <img
+                  alt="blog"
+                  src="https://dummyimage.com/101x101"
+                  class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
+                />
+                <span class="flex-grow flex flex-col pl-3">
+                  <span class="title-font font-medium text-gray-900">Henry Letham</span>
                 </span>
-                <span
-                  class="text-gray-600 inline-flex items-center leading-none text-sm"
-                >
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                    ></path></svg
-                  >6
-                </span>
-              </div>
+              </a>
             </div>
           </div>
         </div>
---
+++ pages/article4.md
@@ -0,0 +1,71 @@
+---
+title: Deep Learning Fundamentals
+lang: en-US
+published: 2019-09-19
+meta:
+  keywords:
+    - TypeScript
+    - JavaScript
+    - Tutorial
+  description:
+    content: Article on TypeScript
+
+sidebar: auto
+---
+
+# MultiClass Text Classification with Scikit-Learn
+
+<br>
+<hr>
+<br>
+
+Let's see the code
+
+```js{2}
+const newVar
+newVar = [1,2,3,4,5]
+console.log(newVar)
+```
+
+This blog covers multiclass classification on a customer complaints dataset.  
+After preprocessing the data we will build multiple models with different estimator and different hyperparemeters to find the best performing model.
+
+<br>
+
+## Get the data ready
+
+As an example dataset, we'll import Consumer_Complaints.csv.
+
+```python
+# Import data and filter only on the needed features an labels
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('drive/My Drive/Colab Notebooks/Consumer_Complaints.csv')
+
+# Select only the Product and Consumer Complaint columns
+col = ['Product', 'Consumer Complaint']
+dataset= dataset[col]
+
+# Drop rows with missing labels
+dataset.dropna(subset=["Consumer Complaint"], inplace=True)
+
+# Rename column
+dataset.columns=['Product', 'ConsumerComplaint']
+
+# Convert the label (Product) to numeric using the pd factorize function
+dataset['category_id'] = dataset['Product'].factorize()[0]
+
+# Only include the following products
+dataset=dataset[dataset['Product'].isin(['Credit reporting',
+                                         'Debt collection',
+                                         'Mortgage',
+                                         'Credit Card',
+                                         'Student loan',
+                                         'Bank account or service'])]
+
+# Print the top 5 rows
+dataset.head()
+```
+
+The Jupyter Notebook can be found here, [GitHub](https://github.com/snymanje/MultiClass-Text-Classification/blob/master/MultiClass_Text_Classification_with_Multiple_Estimators_and_Hyperparameter_Optimization.ipynb)
