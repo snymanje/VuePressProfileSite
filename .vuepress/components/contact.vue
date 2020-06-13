4b262f4f14cbfcd7a0aa926df6edc495ff796255
jeansn <jeansn@tfg.co.za>
2020-06-13 08:04:25 +0200

    updated contact form

--- .vuepress/components/contact.vue
+++ .vuepress/components/contact.vue
@@ -1,61 +1,72 @@
 <template>
-  <section class="text-gray-700 body-font relative">
-    <div class="container px-5 py-24 mx-auto flex sm:flex-no-wrap flex-wrap">
+  <section id="contact" class="text-gray-700 body-font relative">
+    <div>
+      <div class="container mx-auto flex flex-col text-center w-full py-16 px-4">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-800">Contact Me</h1>
+        <span class="bg-green-700 w-12 h-1 flex self-center mb-4"></span>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+          gentrify, subway tile poke farm-to-table.
+        </p>
+      </div>
+    </div>
+
+    <div class="container px-2 pb-20 mx-auto flex sm:flex-no-wrap flex-wrap">
       <div
-        class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"
+        class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-0 sm:p-10 flex items-end justify-start relative"
       >
         <iframe
           width="100%"
           height="100%"
-          class="absolute inset-0"
+          class="absolute inset-0 hidden sm:block"
           frameborder="0"
           title="map"
           marginheight="0"
           marginwidth="0"
           scrolling="no"
-          src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-          style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
+          src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          style="filter: grayscale(0) contrast(1.2) opacity(0.9);"
         ></iframe>
-        <div class="bg-white relative flex flex-wrap py-6">
+        <div class="bg-white relative flex flex-wrap py-6 shadow-lg rounded">
           <div class="lg:w-1/2 px-6">
             <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm">ADDRESS</h2>
-            <p
-              class="leading-relaxed"
-            >Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+            <p class="leading-relaxed">
+              Photo booth tattooed prism, portland taiyaki hoodie neutra
+              typewriter
+            </p>
           </div>
           <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
             <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm">EMAIL</h2>
-            <a class="text-green-500 leading-relaxed">example@email.com</a>
+            <a class="text-indigo-500 leading-relaxed">example@email.com</a>
             <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mt-4">PHONE</h2>
             <p class="leading-relaxed">123-456-7890</p>
           </div>
         </div>
       </div>
-      <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+      <div
+        class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full py-4 md:py-8 mt-8 md:mt-0 px-3 rounded-lg"
+      >
         <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
         <p
           class="leading-relaxed mb-5 text-gray-600"
         >Post-ironic portland shabby chic echo park, banjo fashion axe</p>
         <input
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
+          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
           placeholder="Name"
           type="text"
         />
         <input
-          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2 mb-4"
+          class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
           placeholder="Email"
           type="email"
         />
         <textarea
-          class="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-green-500 text-base px-4 py-2 mb-4 resize-none"
+          class="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none"
           placeholder="Message"
         ></textarea>
         <button
-          class="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+          class="text-white bg-green-700 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
         >SEND</button>
-        <p
-          class="text-xs text-gray-500 mt-3"
-        >Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
       </div>
     </div>
   </section>
