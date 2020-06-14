509935aed94ecfc193584bccacf58feffd8edbc3
jeansn <jeansn@tfg.co.za>
2020-06-14 12:59:57 +0200

    disabled sw

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    plugins: {
-        "@vuepress/pwa": {
-            serviceWorker: true,
-            updatePopup: true,
+        plugins: {
+            "@vuepress/pwa": {
+                serviceWorker: false,
+                updatePopup: false,
+            },
+            "@vuepress/nprogress": true,
+            "@vuepress/back-to-top": true,
+            "@vuepress/medium-zoom": true,
+            "reading-progress": true,
         },
-        "@vuepress/nprogress": true,
-        "@vuepress/back-to-top": true,
-        "@vuepress/medium-zoom": true,
-        "reading-progress": true,
-    },
-    markdown: {
-        lineNumbers: true,
-    },
-    postcss: {
-        plugins: [
-            require("autoprefixer"),
-            require("tailwindcss")("./tailwind.config.js"),
-        ],
-    },
-    title: "Jean Snyman",
-    themeConfig: {
-        nav: [
-            { text: "Home", link: "/" },
-            { text: "Blog", link: "/pages/" },
-        ],
-        searchPlaceholder: "Search...",
-        smoothScroll: true,
-    },
-    shouldPrefetch: () => false,
+        markdown: {
+            lineNumbers: true,
+        },
+        postcss: {
+            plugins: [
+                require("autoprefixer"),
+                require("tailwindcss")("./tailwind.config.js"),
+            ],
+        },
+        title: "Jean Snyman",
+        themeConfig: {
+            nav: [
+                { text: "Home", link: "/" },
+                { text: "Blog", link: "/pages/" },
+            ],
+            searchPlaceholder: "Search...",
+            smoothScroll: true,
+        },
+        /*  shouldPrefetch: () => false,
     shouldPreload: () => false,
-};
+}; */
